7b9b5a814da71ab8a8d773dc3e7ecec69b2ebcbc
Nick Schneeberger <nick.schneeberger@icloud.com>
2023-05-17 17:04:26 +0200

    update

--- js/control.js
+++ js/control.js
@@ -35,51 +35,80 @@ async function deleteData(id) {
     return data;
 }
 
-async function renderTodos() {
+async function renderTodos(loggedIn) {
     const todos = await getData();
     const todosList = document.getElementById('todos-list');
 
     todosList.innerHTML = '';
-    
+
     todos.forEach(todo => {
         const listItem = document.createElement('li');
         listItem.classList.add('todo-item');
         listItem.textContent = todo.todo;
 
-        const editButton = document.createElement('button');
-        editButton.textContent = 'Edit';
-        editButton.addEventListener('click', () => {
-            const newTodo = prompt('Enter new todo:', todo.todo);
-            if (newTodo) {
-                updateData(todo.id, newTodo).then(() => {
-                    listItem.textContent = newTodo;
-                });
-            }
-        });
+        if (loggedIn) {
+            const editButton = document.createElement('button');
+            editButton.textContent = 'Edit';
+            editButton.addEventListener('click', () => {
+                const newTodo = prompt('Enter new todo:', todo.todo);
+                if (newTodo) {
+                    updateData(todo.id, newTodo).then(() => {
+                        listItem.textContent = newTodo;
+                    });
+                }
+            });
 
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = 'Delete';
-        deleteButton.addEventListener('click', () => {
-            if (confirm('Are you sure you want to delete this todo?')) {
-                deleteData(todo.id).then(() => {
-                    todosList.removeChild(listItem);
-                });
-            }
-        });
+            const deleteButton = document.createElement('button');
+            deleteButton.textContent = 'Delete';
+            deleteButton.addEventListener('click', () => {
+                if (confirm('Are you sure you want to delete this todo?')) {
+                    deleteData(todo.id).then(() => {
+                        todosList.removeChild(listItem);
+                    });
+                }
+            });
 
-        listItem.appendChild(editButton);
-        listItem.appendChild(deleteButton);
+            listItem.appendChild(editButton);
+            listItem.appendChild(deleteButton);
+        }
+
         todosList.appendChild(listItem);
     });
 }
 
-document.getElementById('create-button').addEventListener('click', async () => {
-    const newTodo = prompt('Enter new todo:');
-    if (newTodo) {
+function showCreateTodoButton() {
+    const createButton = document.createElement("button");
+    createButton.textContent = "Create Todo";
+    createButton.setAttribute("id", "create-button");
+    createButton.addEventListener("click", async () => {
+      const newTodo = prompt("Enter new todo:");
+      if (newTodo) {
         const createdTodo = await createData(newTodo);
         renderTodos();
+      }
+    });
+    document.body.appendChild(createButton);
+  }
+
+  function removeCreateTodoButton() {
+    const createButton = document.getElementById("create-button");
+    if (createButton) {
+      document.body.removeChild(createButton);
     }
-});
+  }
+
+function showLoginButton() {
+    const loginButton = document.createElement('button');
+    loginButton.textContent = 'Login to create, update and delete todos';
+    loginButton.addEventListener('click', () => {
+        const email = prompt('Please enter your email to receive a magic link:');
+        if (email) {
+            signInWithEmail(email);
+        }
+    });
+
+    document.body.appendChild(loginButton);
+}
 
 async function signInWithEmail(email) {
     const { error } = await supa.auth.signIn({ email });
@@ -92,13 +121,15 @@ async function signInWithEmail(email) {
   
   async function checkAuth() {
     const user = supa.auth.user();
-    if (!user) {
-      const email = prompt('Please enter your email to receive a magic link:');
-      if (email) {
-        signInWithEmail(email);
-      }
+    if (user) {
+      showCreateTodoButton();
+      showLogoutButton();
+      renderTodos(true);
     } else {
-      renderTodos();
+      removeCreateTodoButton();
+      removeLogoutButton();
+      showLoginButton();
+      renderTodos(false);
     }
   }
   
@@ -110,5 +141,28 @@ async function signInWithEmail(email) {
     }
   });
   
+
+  function showLogoutButton() {
+    const logoutButton = document.createElement("button");
+    logoutButton.textContent = "Logout";
+    logoutButton.setAttribute("id", "logout-button");
+    logoutButton.addEventListener("click", async () => {
+      await supa.auth.signOut();
+      removeLogoutButton();
+      removeCreateTodoButton();
+      showLoginButton();
+      renderTodos(false);
+    });
+  
+    document.body.appendChild(logoutButton);
+  }
+
+  function removeLogoutButton() {
+    const logoutButton = document.getElementById("logout-button");
+    if (logoutButton) {
+      document.body.removeChild(logoutButton);
+    }
+  }
+
 
 checkAuth();
